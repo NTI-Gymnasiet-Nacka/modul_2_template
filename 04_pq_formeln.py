711de6c7eb1051d9b3042be6514ca2f1920994d0
Niclas L <niclas.lund@ntig.se>
2022-08-25 09:03:27 +0200

    Uppdaterade uppgiftsbeskrivning med complex.

--- 04_pq_formeln.py
+++ 04_pq_formeln.py
@@ -7,5 +7,5 @@
 # x^2 + px + q = 0
 # Skriv ut vad x_1 samt x_2 blir.
 # Denna uppgift kräver inte att ekvationer med imaginär lösning
-# redovisas. Endast reella rötter.
+# redovisas. Om du vill kan du även hantera komplexa lösningar. Kolla då upp variabeltypen complex.
 # Kod nedan
